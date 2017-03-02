dd4b50986bd30e3336a411b4bf315919290f591f
Alexander Mostovenko <alexmost1989@gmail.com>
2017-03-03 00:19:07 +0200

    added loadFile helper method

--- src/loader.js
+++ src/loader.js
@@ -3,21 +3,22 @@ import path from 'path';
 import gParser from 'gettext-parser';
 import { addLocale } from './index';
 
+export function loadFile(filepath) {
+    const fileContent = fs.readFileSync(filepath);
+    if (path.extname(filepath) === '.mo') {
+        return gParser.mo.parse(fileContent);
+    }
+
+    if (path.extname(filepath) === '.po') {
+        return gParser.po.parse(fileContent);
+    }
+
+    throw new Error(`Unsupported filetype ${filepath}`);
+}
+
 export function loadLocale(locale, filepath) {
     if (typeof filepath === 'string') {
-        const fileContent = fs.readFileSync(filepath);
-        let data;
-        if (path.extname(filepath) === '.mo') {
-            data = gParser.mo.parse(fileContent);
-            return addLocale(locale, data);
-        }
-
-        if (path.extname(filepath) === '.po') {
-            data = gParser.po.parse(fileContent);
-            return addLocale(locale, data);
-        }
-
-        throw new Error(`Unsupported filetype ${filepath}`);
+        return addLocale(locale, loadFile(filepath));
     }
 
     if (filepath && typeof filepath === 'object' &&
