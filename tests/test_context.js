dca821b8b95a2d9084b8513d221d4db02f26ab9b
Alexander Mostovenko <alexmost1989@gmail.com>
2017-10-27 00:08:20 +0300

    added tests for context for ngettext, jt and gettext

--- tests/test_context.js
+++ tests/test_context.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { t, c, useLocale } from '../src/index';
+import { t, jt, c, useLocale, ngettext, msgid, gettext } from '../src/index';
 import { loadLocale } from '../src/loader';
 
 describe('contexts', () => {
@@ -16,6 +16,15 @@ describe('contexts', () => {
                             'Click ${ 1 } and ${ 0 } ctx default',
                         ],
                     },
+                    '${ 0 } hour': {
+                        msgid: '${ 0 } hour',
+                        msgid_plural: '${ 0 } hours',
+                        msgstr: ['${ 0 } hour default', '${ 0 } hours default'],
+                    },
+                    test: {
+                        msgid: 'test',
+                        msgstr: ['test default'],
+                    },
                 },
                 ctx2: {
                     'Click ${ 0 } and ${ 1 }': {
@@ -24,6 +33,15 @@ describe('contexts', () => {
                             'Click ${ 1 } and ${ 0 } ctx2',
                         ],
                     },
+                    '${ 0 } hour': {
+                        msgid: '${ 0 } hour',
+                        msgid_plural: '${ 0 } hours',
+                        msgstr: ['${ 0 } hour ctx2', '${ 0 } hours ctx2'],
+                    },
+                    test: {
+                        msgid: 'test',
+                        msgstr: ['test ctx2'],
+                    },
                 },
             },
         };
@@ -31,12 +49,41 @@ describe('contexts', () => {
         useLocale('en2');
     });
 
-    it('should resolve translation from the context', () => {
+    it('should resolve translation from the context for t', () => {
         const result1 = c('ctx2').t`Click ${100} and ${200}`;
         expect(result1).to.eql('Click 200 and 100 ctx2');
 
         const result2 = t`Click ${100} and ${200}`;
         expect(result2).to.eql('Click 200 and 100 ctx default');
+    });
+
+    it('should resolve translation from the context for jt', () => {
+        const result1 = c('ctx2').jt`Click ${100} and ${200}`;
+        expect(result1).to.eql(['Click ', 200, ' and ', 100, ' ctx2']);
+
+        const result2 = jt`Click ${100} and ${200}`;
+        expect(result2).to.eql(['Click ', 200, ' and ', 100, ' ctx default']);
+    });
+
+    it('should resolve translation from the context for the ngettext', () => {
+        const n = 1;
+        const result1 =
+            c('ctx2')
+            .ngettext(msgid`${n} hour`, `${n} hours`, n);
+
+        expect(result1).to.eql('1 hour ctx2');
+
+        const result2 = ngettext(msgid`${n} hour`, `${n} hours`, n);
+        expect(result2).to.eql('1 hour default');
+    });
+
+    it('should resolve translation from the context for the gettext', () => {
+        const result1 = c('ctx2').gettext('test');
+
+        expect(result1).to.eql('test ctx2');
+
+        const result2 = gettext('test');
+        expect(result2).to.eql('test default');
     });
 });
 
