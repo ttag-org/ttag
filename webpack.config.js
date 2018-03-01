b3f7a0e306c57f91c527ab1843df03b7c4839040
a.mostovenko <a.mostovenko@smartweb.com.ua>
2018-03-01 18:52:38 +0200

    skip node_modules processing for build

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     module: {
         rules: [
-            {test: /\.(js|jsx)$/, use: 'babel-loader'}
+            { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ }
         ]
     }
 };
