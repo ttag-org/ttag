8190dbc5ce35f80fbbb574499cd9c6335122ac47
a.mostovenko <a.mostovenko@smartweb.com.ua>
2018-03-01 19:00:36 +0200

    added define plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
+const webpack = require('webpack');
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -11,5 +13,12 @@ module.exports = {
         rules: [
             { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            }
+        }),
+    ]
 };
