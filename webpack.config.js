dc66e15c37c43be016f1b15fc6f192868a60e237
Alexander Mostovenko <alexmost1989@gmail.com>
2018-05-04 22:12:48 +0300

    change resulting bundle name c-3po.js -> ttag.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: isProd ? 'c3po.min.js':  'c3po.js',
+        filename: isProd ? 'ttag.min.js':  'ttag.js',
         libraryTarget: 'umd'
     },
     module: {
