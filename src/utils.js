67990cf006ebf163ef559e6c39e2dab3474f41cd
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-06-04 11:21:09 -0400

    allow exprs to contain $1 and $2

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,7 @@ const memoize1 = (f) => (arg) => {
     return mem[arg];
 };
 
-const reg = (i) => new RegExp(`\\$\\{([\\s]+?|\\s?)${i}([\\s]+?|\\s?)}`);
+const reg = (i) => new RegExp(`\\$\\{(?:[\\s]+?|\\s?)${i}(?:[\\s]+?|\\s?)}`);
 const memReg = memoize1(reg);
 
 export const msgid2Orig = (id, exprs) => {
