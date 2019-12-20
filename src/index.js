e30ed3aabcd245719e6a28ed520486e947c32632
Alex R <alexei.rudenko@gmail.com>
2019-12-20 18:40:32 +0100

    Support multiple ttag instances

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { validateNgettextMsgid, validateNgettextNumber,
     validateNgettextPluralForms, validateLang } from './validation';
 import Config from './config';
 
-const conf = new Config();
-
 function Context(context) {
     if (process.env.NODE_ENV !== 'production') {
         if (typeof context !== 'string') {
@@ -37,79 +35,8 @@ function hasTranslations(msgstr) {
     return true;
 }
 
-function findTransObj(locale, str, ctx) {
-    const locales = conf.getAvailLocales();
-    const localeData = locales[locale];
-    if (!localeData) return null;
-    // verbose format
-    if (localeData.translations) {
-        const translations = localeData.translations[ctx] || localeData.translations[''];
-        const translation = translations && translations[str];
-        if (translation && !isFuzzy(translation) && hasTranslations(translation.msgstr)) {
-            return translation.msgstr;
-        }
-    }
-    // compact format
-    if (localeData.contexts) {
-        const translations = localeData.contexts[ctx] || localeData.contexts[''];
-        const translation = translations && translations[str];
-        if (translation && hasTranslations(translation)) {
-            return translation;
-        }
-    }
-    return null;
-}
-
-function findTranslation(str, ctx) {
-    const locales = conf.getCurrentLocales();
-    if (locales.length) {
-        for (let i = 0; i < locales.length; i++) {
-            const translation = findTransObj(locales[i], str, ctx);
-            if (translation) {
-                conf.setCurrentLocale(locales[i]);
-                return translation;
-            }
-        }
-    }
-    return findTransObj(conf.getCurrentLocale(), str, ctx);
-}
-
-const maybeDedent = (str) => (conf.isDedent() ? dedentStr(str) : str);
-
-export function t(strings, ...exprs) {
-    let result = strings;
-    if (strings && strings.reduce) {
-        const id = maybeDedent(getMsgid(strings, exprs));
-        const context = getTransContext(this);
-        const trans = findTranslation(id, context);
-        result = trans ?
-            msgid2Orig(trans[0], exprs) : buildStr(strings, exprs);
-    }
-    return maybeDedent(result);
-}
-
 const separator = /(\${\s*\d+\s*})/g;
 const slotIdRegexp = /\${\s*(\d+)\s*}/;
-
-export function jt(strings, ...exprs) {
-    if (strings && strings.reduce) {
-        const id = maybeDedent(getMsgid(strings, exprs));
-        const context = getTransContext(this);
-        const trans = findTranslation(id, context);
-        if (!trans) return buildArr(strings, exprs);
-
-        // splits string & capturing group into tokens
-        //
-        const translatedTokens = trans[0].split(separator);
-
-        return translatedTokens.map((token) => {
-            const slotIdMatch = token.match(slotIdRegexp);
-            // slotIdMatch is not null only when the token is a variable slot (${xx})
-            return slotIdMatch ? exprs[+slotIdMatch[1]] : token;
-        });
-    }
-    return strings;
-}
 
 export function msgid(strings, ...exprs) {
     /* eslint-disable no-new-wrappers */
@@ -123,65 +50,169 @@ export function msgid(strings, ...exprs) {
     return strings;
 }
 
-export function gettext(id) {
-    const context = getTransContext(this);
-    const trans = findTranslation(id, context);
-    return trans ? trans[0] : id;
-}
+export function TTag() {
+    const conf = new Config();
 
-export const _ = gettext;
+    const maybeDedent = (str) => (conf.isDedent() ? dedentStr(str) : str);
 
-export function ngettext(...args) {
-    if (process.env.NODE_ENV !== 'production') validateNgettextMsgid(args[0]);
+    const findTransObj = (locale, str, ctx) => {
+        const locales = conf.getAvailLocales();
+        const localeData = locales[locale];
+        if (!localeData) return null;
+        // verbose format
+        if (localeData.translations) {
+            const translations = localeData.translations[ctx] || localeData.translations[''];
+            const translation = translations && translations[str];
+            if (translation && !isFuzzy(translation) && hasTranslations(translation.msgstr)) {
+                return translation.msgstr;
+            }
+        }
+        // compact format
+        if (localeData.contexts) {
+            const translations = localeData.contexts[ctx] || localeData.contexts[''];
+            const translation = translations && translations[str];
+            if (translation && hasTranslations(translation)) {
+                return translation;
+            }
+        }
+        return null;
+    };
 
-    const id = maybeDedent(getMsgid(args[0]._strs, args[0]._exprs));
-    const n = args[args.length - 1];
-    if (process.env.NODE_ENV !== 'production') validateNgettextNumber(n);
+    const findTranslation = (str, ctx) => {
+        const locales = conf.getCurrentLocales();
+        if (locales.length) {
+            for (let i = 0; i < locales.length; i++) {
+                const translation = findTransObj(locales[i], str, ctx);
+                if (translation) {
+                    conf.setCurrentLocale(locales[i]);
+                    return translation;
+                }
+            }
+        }
+        return findTransObj(conf.getCurrentLocale(), str, ctx);
+    };
 
-    const forms = args.slice(1, -1);
-    forms.unshift(args[0].toString());
-    if (process.env.NODE_ENV !== 'production') {
-        validateNgettextPluralForms(conf.getDefaultPluralFormsCount(), forms.length);
+    function setDefaultLang(lang) {
+        if (process.env.NODE_ENV !== 'production') validateLang(lang);
+        conf.setDefaultLang(lang);
     }
 
-    const trans = findTranslation(id, getTransContext(this));
-    if (trans) {
-        const pluralFn = getPluralFnForTrans(conf);
-        return maybeDedent(
-            msgid2Orig(pluralFn(n, trans), args[0]._exprs)
-        );
+    function useLocales(locales) {
+        conf.setCurrentLocales(locales);
     }
-    const pluralFn = conf.getDefaultPluralFn();
-    return maybeDedent(pluralFn(n, forms));
-}
 
-export function addLocale(locale, data) {
-    conf.addLocale(locale, data);
-}
+    function setDedent(value) {
+        conf.setDedent(Boolean(value));
+    }
 
-export function useLocale(locale) {
-    conf.setCurrentLocale(locale);
-}
+    function useLocale(locale) {
+        conf.setCurrentLocale(locale);
+    }
 
-export function setDedent(value) {
-    conf.setDedent(Boolean(value));
-}
+    function addLocale(locale, data) {
+        conf.addLocale(locale, data);
+    }
 
-export function useLocales(locales) {
-    conf.setCurrentLocales(locales);
-}
+    function t(strings, ...exprs) {
+        let result = strings;
+        if (strings && strings.reduce) {
+            const id = maybeDedent(getMsgid(strings, exprs));
+            const context = getTransContext(this);
+            const trans = findTranslation(id, context);
+            result = trans ?
+                msgid2Orig(trans[0], exprs) : buildStr(strings, exprs);
+        }
+        return maybeDedent(result);
+    }
 
-export function setDefaultLang(lang) {
-    if (process.env.NODE_ENV !== 'production') validateLang(lang);
-    conf.setDefaultLang(lang);
-}
+    function jt(strings, ...exprs) {
+        if (strings && strings.reduce) {
+            const id = maybeDedent(getMsgid(strings, exprs));
+            const context = getTransContext(this);
+            const trans = findTranslation(id, context);
+            if (!trans) return buildArr(strings, exprs);
 
-export function c(context) {
-    const ctx = new Context(context);
+            // splits string & capturing group into tokens
+            //
+            const translatedTokens = trans[0].split(separator);
+
+            return translatedTokens.map((token) => {
+                const slotIdMatch = token.match(slotIdRegexp);
+                // slotIdMatch is not null only when the token is a variable slot (${xx})
+                return slotIdMatch ? exprs[+slotIdMatch[1]] : token;
+            });
+        }
+        return strings;
+    }
+
+    function ngettext(...args) {
+        if (process.env.NODE_ENV !== 'production') validateNgettextMsgid(args[0]);
+
+        const id = maybeDedent(getMsgid(args[0]._strs, args[0]._exprs));
+        const n = args[args.length - 1];
+        if (process.env.NODE_ENV !== 'production') validateNgettextNumber(n);
+
+        const forms = args.slice(1, -1);
+        forms.unshift(args[0].toString());
+        if (process.env.NODE_ENV !== 'production') {
+            validateNgettextPluralForms(conf.getDefaultPluralFormsCount(), forms.length);
+        }
+
+        const trans = findTranslation(id, getTransContext(this));
+        if (trans) {
+            const pluralFn = getPluralFnForTrans(conf);
+            return maybeDedent(
+                msgid2Orig(pluralFn(n, trans), args[0]._exprs)
+            );
+        }
+        const pluralFn = conf.getDefaultPluralFn();
+        return maybeDedent(pluralFn(n, forms));
+    }
+
+    function gettext(id) {
+        const context = getTransContext(this);
+        const trans = findTranslation(id, context);
+        return trans ? trans[0] : id;
+    }
+
+    const _ = gettext;
+
+    function c(context) {
+        const ctx = new Context(context);
+        return {
+            t: t.bind(ctx),
+            jt: jt.bind(ctx),
+            gettext: gettext.bind(ctx),
+            ngettext: ngettext.bind(ctx),
+        };
+    }
+
     return {
-        t: t.bind(ctx),
-        jt: jt.bind(ctx),
-        gettext: gettext.bind(ctx),
-        ngettext: ngettext.bind(ctx),
+        _,
+        addLocale,
+        c,
+        gettext,
+        jt,
+        ngettext,
+        setDedent,
+        setDefaultLang,
+        t,
+        useLocale,
+        useLocales,
     };
 }
+
+const globalTTag = new TTag();
+
+export const _ = globalTTag._;
+export const addLocale = globalTTag.addLocale;
+export const c = globalTTag.c;
+export const gettext = globalTTag.gettext;
+export const jt = globalTTag.jt;
+export const ngettext = globalTTag.ngettext;
+export const setDedent = globalTTag.setDedent;
+export const setDefaultLang = globalTTag.setDefaultLang;
+export const t = globalTTag.t;
+export const useLocale = globalTTag.useLocale;
+export const useLocales = globalTTag.useLocales;
+
