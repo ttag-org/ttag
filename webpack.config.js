1d62edc7983fda4dbc22aa0fc591e8f80cbf2688
a.mostovenko <alexmost1989@gmail.com>
2023-12-06 14:08:33 +0100

    Fix conflicting process.env variable

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'dev'),
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
             },
         }),
     ],
