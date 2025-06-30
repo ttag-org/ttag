2b64fac88dcb37f35052ee6af74cdaa4682e737f
alexmost <alexmost1989@gmail.com>
2025-06-30 12:04:36 +0200

    fix ts typings for esnext module resolution

--- Global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'plural-forms/minimal-safe';
-
-declare module 'dedent' {
-    export function dedent(literals: string): string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    export function dedent(literals: TemplateStringsArray, ...placeholders: any[]): string;
-    export default dedent;
-}
